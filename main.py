4a196b6148b49eb9ac8e723daaeb4a12cdb9a74d
mc_fdc <Masato.04.11.2007@gmail.com>
2022-08-05 22:54:34 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class tuna2134:
+    def __init__(self):
+        self.nickname: str = "mc_fdc"
+        self.old: int = 15
+        self.country_of_residence: str = "Japan"
+        self.languages: List[str] = ["English", "Japan"]
+        self.foods: List[str] = ["Water melon", "Melon", "Orange"]
+        self.website: str = "https://mc-fdc.com"
